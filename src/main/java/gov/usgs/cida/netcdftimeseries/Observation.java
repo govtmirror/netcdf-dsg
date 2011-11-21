de0dc40de90b78f3c03ce19264d09b737eae8eb4
jiwalker <jiwalker@622dc1fb-2224-0410-ab72-dc85a3975e1a>
2011-11-21 23:59:37 +0000

    First Import
    git-svn-id: https://cida-svn.er.usgs.gov/repos/dev/usgs/netcdf/netcdf-timeseries/trunk@18868 622dc1fb-2224-0410-ab72-dc85a3975e1a

---
+++ src/main/java/gov/usgs/cida/netcdftimeseries/Observation.java
@@ -0,0 +1,21 @@
+package gov.usgs.cida.netcdftimeseries;
+/**
+ *
+ * @author Jordan Walker <jiwalker@usgs.gov>
+ */
+public class Observation {
+    
+    public final int time_offset;
+    public final int station_index;
+    public final Object[] values;
+    
+    /*
+     * Take time as an offset from some time
+     * TODO take a DateTime from Joda and convert it to index here or in record
+     */
+    public Observation(int time, int index, Object... values) {
+        this.time_offset = time;
+        this.station_index = index;
+        this.values = values;
+    } 
+}
